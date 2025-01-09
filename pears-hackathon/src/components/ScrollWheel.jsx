0cbc57f8ac27aca602c69ed82574694e681e0f78
Darsh Jain <132695189+sparky0520@users.noreply.github.com>
2025-01-09 19:15:25 +0530

    fixed nav wheel

---
+++ pears-hackathon/src/components/ScrollWheel.jsx
@@ -0,0 +1,28 @@
+"use client";
+import React from "react";
+
+function loadSVG() {
+  fetch("/navigation_wheel.svg")
+    .then((response) => {
+      return response.text();
+    })
+    .then((svg) => {
+      document.getElementById("scroll_wheel").innerHTML = svg;
+      document.querySelector("#scroll_wheel svg").classList.add("fixed");
+      document.querySelector("#scroll_wheel svg").classList.add("right-[-20%]");
+      document.querySelector("#scroll_wheel svg").classList.add("h-[100vh]");
+      document.querySelector("#scroll_wheel svg").classList.add("w-[100vh]");
+
+      document
+        .querySelector("#scroll_wheel svg")
+        .setAttribute("preserveAspectRatio", "xMidYMid slice");
+      setAnimationScroll();
+    });
+}
+loadSVG();
+
+function ScrollWheel() {
+  return <div id="scroll_wheel"></div>;
+}
+
+export default ScrollWheel;
